52284318d8ff3b323d786c9bac04df7be1c49022
Sean Coughlin <coughlinscc33@gmail.com>
2023-10-06 14:25:31 -0500

    Format with prettier

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,46 +1,37 @@
 <footer
-    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+  class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
-    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
-        Connect with me &darr;
+  <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+    Connect with me &darr;
+  </p>
+  <div class="flex flex-col gap-4 items-center justify-center">
+    <p><b class="pr-2">Email</b> coughlinscc33@gmail.com</p>
+    <p>
+      <b class="pr-2">GitHub</b>
+      <a href="https://github.com/Scc33" target="_blank" class="text-violet-400"
+        >Scc33<sup class=""
+          ><span class="text-xs scale-75 pl-0.5"
+            ><i
+              class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+            /></span
+          ></sup
+        ></a
+      >
     </p>
-    <div class="flex flex-col gap-4 items-center justify-center">
-        <!-- <div
-            class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
-        >
-            <a>
-                <i class="fa-brands fa-linkedin-in" />
-            </a>
-            <a>
-                <i class="fa-brands fa-github" />
-            </a>
-        </div>
-        <p class="mx-auto text-sm opacity-70">or</p>
-        <p class="">Hugh_jass@gmail.com</p> -->
-        <p><b class="pr-2">Email</b> samuel_oak@gmail.com</p>
-        <p>
-            <b class="pr-2">GitHub</b>
-            <a href=" " target="_blank" class="text-violet-400"
-                >samuel_oak<sup class=""
-                    ><span class="text-xs scale-75 pl-0.5"
-                        ><i
-                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
-                        /></span
-                    ></sup
-                ></a
-            >
-        </p>
-        <p>
-            <b class="pr-2">LinkedIn</b>
-            <a href=" " target="_blank" class="text-violet-400"
-                >samuel_oak<sup class=""
-                    ><span class="text-xs scale-75 pl-0.5"
-                        ><i
-                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
-                        /></span
-                    ></sup
-                ></a
-            >
-        </p>
-    </div>
+    <p>
+      <b class="pr-2">LinkedIn</b>
+      <a
+        href="https://www.linkedin.com/in/sean-m-coughlin/"
+        target="_blank"
+        class="text-violet-400"
+        >sean-m-coughlin<sup class=""
+          ><span class="text-xs scale-75 pl-0.5"
+            ><i
+              class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+            /></span
+          ></sup
+        ></a
+      >
+    </p>
+  </div>
 </footer>
