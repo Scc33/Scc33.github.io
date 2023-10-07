ee8e156fed850078870b379b8c5c8045871eb424
Sean Coughlin <coughlinscc33@gmail.com>
2023-10-07 11:32:22 -0500

    Work on configurations

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+    kit: {
+        adapter: adapter()
+    },
+    paths: {
+        base: dev ? '' : process.env.BASE_PATH,
+    },
+    preprocess: vitePreprocess()
 };
 export default config;
