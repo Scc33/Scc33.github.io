57973bf09ca9179a96a92d9e63a5dd1beec22da2
Sean Coughlin <coughlinscc33@gmail.com>
2023-10-07 12:08:06 -0500

    Copy CNAME to build file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-    plugins: [sveltekit()]
+    plugins: [sveltekit(), viteStaticCopy({
+        targets: [{
+            src: './CNAME',
+            dest: ''
+        }]
+    })]
 });
