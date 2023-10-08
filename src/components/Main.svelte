9c3aaaea1f88c54cfdc9f6a658d41747a8804250
Sean Coughlin <coughlinscc33@gmail.com>
2023-10-08 08:11:46 -0500

    Speed up page loads by shrinking assets

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -42,7 +42,7 @@
     </div>
     <div class="relative shadow-2xl grid place-items-center">
       <img
-        src={"images/profile.png"}
+        src={"images/profile.webp"}
         alt="Zetane Engine"
         class="object-cover z-[2] max-h-[70vh]"
       />
