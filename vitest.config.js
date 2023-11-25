8eaff09d2c9c1f61956524f709083fa02b89786a
Sean Coughlin <coughlinscc33@gmail.com>
2023-11-25 11:57:43 -0600

    Create test coverage on footer and main file

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
     plugins: [svelte({ hot: !process.env.VITEST })],
     test: {
-        globals: false,
+        globals: true,
         environment: 'jsdom',
         setupFiles: ['./vitest-setup.js'],
     },
