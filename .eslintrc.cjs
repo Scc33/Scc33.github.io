d997da1efc19431c16214701b62ccbdb1ee6d9b9
Sean Coughlin <coughlinscc33@gmail.com>
2023-11-26 20:38:36 -0600

    update lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,23 +6,11 @@ module.exports = {
     },
     plugins: ["vitest"],
     extends: [
-        'standard',
-        'prettier',
-        'plugin:vitest/recommended',
+        "eslint:recommended",
+        'plugin:vitest/all',
         'plugin:svelte/recommended',
         "plugin:vitest-globals/recommended"
     ],
-    overrides: [{
-        env: {
-            node: true
-        },
-        files: [
-            '.eslintrc.{js,cjs}'
-        ],
-        parserOptions: {
-            sourceType: 'script'
-        }
-    }],
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module'
