747ae968225ef4d4a552e96bba58dcbbd639b72d
Sean Coughlin <coughlinscc33@gmail.com>
2023-12-08 18:12:31 -0600

    Increase Playwright workers in CI

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   reporter: "html",
   use: {
     trace: "on-first-retry",
