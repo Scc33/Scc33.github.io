5f83a71db121dc9b7774a574300e12d547d162e6
Sean Coughlin <coughlinscc33@gmail.com>
2024-04-03 16:20:18 -0500

    Switch scripts to Bun

--- playwright.config.js
+++ playwright.config.js
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "yarn run start --port 4173",
+    command: "bun run start --port 4173",
     url: "http://localhost:4173",
     reuseExistingServer: !process.env.CI,
     stdout: "pipe"
