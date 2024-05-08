87802f19c78a8f7be94cba343c1655f6d92405fa
Sean Coughlin <coughlinscc33@gmail.com>
2024-05-08 07:25:42 -0500

    Debug output processing

--- scripts/lighthouse/index.js
+++ scripts/lighthouse/index.js
@@ -13,6 +13,7 @@ const formatScore = (res) => {
  */
 export const formatLighthouseResults = ({ core }) => {
   // this will be the shape of https://github.com/treosh/lighthouse-ci-action#manifest
+  console.log(process.env.LIGHTHOUSE_RESULT);
   const resultsJson = JSON.parse(process.env.LIGHTHOUSE_RESULT);
 
   // start creating our markdown table
