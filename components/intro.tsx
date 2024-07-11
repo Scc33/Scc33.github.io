9fcb0720ea663e3f9578d412f139590a092854c3
Sean Coughlin <coughlinscc33@gmail.com>
2024-07-11 12:24:24 -0500

    Update packages and metadata

--- components/intro.tsx
+++ components/intro.tsx
@@ -58,50 +58,52 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Sean.</span> I'm a{" "}
+        <span className="font-bold">Hello, I&apos;m Sean.</span> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">3 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is React
         (Next.js).
       </motion.h1>
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className="flex flex-row flex-wrap items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
           delay: 0.1
         }}
       >
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com/sean-m-coughlin"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/scc33"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
-        <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://blog.seancoughlin.me"
-          target="_blank"
-        >
-          Blog
-          <CgWebsite className="opacity-60" />
-        </a>
-        <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://portfolio.seancoughlin.me"
-          target="_blank"
-        >
-          Portfolio
-          <LuFolder className="opacity-60" />
-        </a>
+          <a
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+            href="https://www.linkedin.com/in/sean-m-coughlin/"
+            target="_blank"
+          >
+            LinkedIn
+            <BsLinkedin />
+          </a>
+          <a
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+            href="https://github.com/scc33"
+            target="_blank"
+          >
+            GitHub
+            <FaGithubSquare />
+          </a>
+          <a
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+            href="https://blog.seancoughlin.me"
+            target="_blank"
+          >
+            Blog
+            <CgWebsite className="opacity-60" />
+          </a>
+          <a
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+            href="https://portfolio.seancoughlin.me"
+            target="_blank"
+          >
+            Portfolio
+            <LuFolder className="opacity-60" />
+          </a>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/Resume.pdf"
