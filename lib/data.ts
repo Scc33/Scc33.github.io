ca9e8bd7f1be8672b93296765be005adda0e5fa9
Sean Coughlin <coughlinscc33@gmail.com>
2025-06-21 15:38:40 -0500

    Refactor global styles, enhance font loading, and optimize component imports
    - Updated the conic gradient definition in globals.css for improved readability.
    - Added 'display: swap' to the Inter font configuration in layout.tsx for better loading performance.
    - Converted Skills and VerticalHistory components to dynamic imports in page.tsx to enhance loading experience.
    - Removed unused Project and Projects components along with their associated data from data.ts.
    - Deleted obsolete project images from the public directory.

--- lib/data.ts
+++ lib/data.ts
@@ -4,9 +4,6 @@ import { CiCreditCard1 } from "react-icons/ci";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import { SiSpringboot } from "react-icons/si";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 
 type data = {
   title: string;
@@ -27,10 +24,6 @@ export const links = [
     name: "About",
     hash: "#about"
   },
-  /*{
-    name: "Projects",
-    hash: "#projects"
-  },*/
   {
     name: "Skills",
     hash: "#skills"
@@ -100,30 +93,6 @@ export const experiencesData: data[] = [
     date: "2021",
     skills: ["Algorithms", "Data Structures", "Web Development", "Economics"],
     institution: "University of Illinois Urbana-Champaign"
-  }
-] as const;
-
-export const projectsData = [
-  {
-    title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg
-  },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg
   }
 ] as const;
 
