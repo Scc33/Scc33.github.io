82a4ce4840217417f4401f05444b25ac4d473929
Sean Coughlin <coughlinscc33@gmail.com>
2025-06-22 09:22:06 -0500

    Remove unused "use client" directive from SectionDivider component for cleaner code.

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 export default function SectionDivider() {
   return (
     <div className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20 animate-fade-in-delay-125"></div>
