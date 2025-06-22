1a583e30af1704b00adbca0ad7fa347b1a40e64c
Sean Coughlin <coughlinscc33@gmail.com>
2025-06-22 10:10:47 -0500

    Refactor VerticalHistory component to eliminate unnecessary Fragment wrapper around VerticalTimelineElement for cleaner code.

--- components/VerticalHistory.tsx
+++ components/VerticalHistory.tsx
@@ -3,7 +3,6 @@
 import SectionHeading from "./section-heading";
 import { VerticalTimeline, VerticalTimelineElement } from "./VerticalTimeline";
 import { useSectionInView } from "@/lib/hooks";
-import { Fragment } from "react";
 import { experiencesData } from "@/lib/data";
 
 export default function Experience() {
@@ -18,30 +17,29 @@ export default function Experience() {
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline>
         {experiencesData.map((item, index) => (
-          <Fragment key={index}>
-            <VerticalTimelineElement
-              date={item.date}
-              icon={item.icon}
-              index={index}
-              isLast={index === experiencesData.length - 1}
-            >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75 mb-2">
-                {item.description}
-              </p>
-              <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-                {item.skills.map((skill, index) => (
-                  <li
-                    className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                    key={index}
-                  >
-                    {skill}
-                  </li>
-                ))}
-              </ul>
-            </VerticalTimelineElement>
-          </Fragment>
+          <VerticalTimelineElement
+            date={item.date}
+            icon={item.icon}
+            index={index}
+            isLast={index === experiencesData.length - 1}
+            key={index}
+          >
+            <h3 className="font-semibold capitalize">{item.title}</h3>
+            <p className="font-normal !mt-0">{item.location}</p>
+            <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75 mb-2">
+              {item.description}
+            </p>
+            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+              {item.skills.map((skill, index) => (
+                <li
+                  className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                  key={index}
+                >
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </VerticalTimelineElement>
         ))}
       </VerticalTimeline>
     </section>
